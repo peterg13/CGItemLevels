a92154cca3e4d5e3d8bd89b3fa1845da5edc60dd
peterg13 <petergustafson19@yahoo.com>
2017-08-23 23:21:33 -0700

    working on the update function

--- Main.js
+++ Main.js
@@ -10,39 +10,66 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+//allows the server to listen over a port
 app.listen(3000, function () {
   console.log('Server is listening on port 3000')
 })
 
-
-var names = ['tankadinn', 'kaol', 'brightsidesh'];
-var realms = ['darkspear', 'darkspear', 'darkspear'];
-var ilvls = [0, 0, 0];
-
 //called when document is ready
+//will send the client a json which consist of all of the entries in the 'database'
 app.get('/pullTable', function(req, res){
 	//pulls up each entry in our csv file and stores it in an array
 	var csvFilePath = './ilvlData.csv';
 	fs.readFile(csvFilePath, 'UTF-8', function(err, csv) {
 		$.csv.toArrays(csv, {}, function(err, data) {
 			//each line in data[] is an entry
-			var entries = data.length;
 			var finalJSON = [];
-			for(i = 0; i < entries; i++){
+			//converts each entry in data[] into a json object and concats it with 'finalJSON' which will be sent to client
+			for(i = 0; i < data.length; i++){
 				var element = data[i].toString().split(',');
 				var tempJSON = [{name: element[0], realm: element[1], ilvl: element[2]}];
 				finalJSON = finalJSON.concat(tempJSON);
 			}
+			//sends the final json to the client
 			res.json(finalJSON);
-
-			//var element = data[0].toString().split(',');
-			//res.json({name: element[0], realm: element[1], ilvl: element[2]});
     	});
   	});
 })
 
+
+
+var names = ['tankadinn', 'kaol', 'brightsidesh'];
+var realms = ['darkspear', 'darkspear', 'darkspear'];
+var ilvls = [0, 0, 0];
+
+
+//called when the user clicks the update button
+//this will query each character in the database from blizards site and update the database.
 app.get('/update', function(req, res){
-	console.log('update');
+	//pulls up each entry in our csv file and stores it in an array
+	var csvFilePath = './ilvlData.csv';
+	fs.readFile(csvFilePath, 'UTF-8', function(err, csv) {
+		$.csv.toArrays(csv, {}, function(err, data) {
+			//each line in data[] is an entry
+			var entries = data.length;
+			var completed = 0;
+			for(i = 0; i < entries; i++){
+				var element = data[i].toString().split(',');
+				characterRequest(element[0], element[1]).then(response => {
+					completed++;
+				});
+			}
+			while(completed < entries){
+				
+			}
+			res.send('updated');
+    	});
+  	});
+
+
+
+
+	
 })
 
 
@@ -53,13 +80,15 @@ function getItemLevels(names, realms){
 	}
 
 
-function characterRequest(charName, charRealm){
+var characterRequest = function(charName, charRealm){
 	const blizzard = require('blizzard.js').initialize({ apikey: 'ce8c5e2zj8t8q2ebjck9y73usfp2zpt9'});
 
 	blizzard.wow.character(['items'], { origin: 'us', realm: charRealm, name: charName })
   		.then(response => {
     	//console.log(charName + ': ' + response.data.items.averageItemLevelEquipped);
-    	console.log('reponse: ' + charName + ': ' + response.data.items.averageItemLevelEquipped);
+    	//console.log('reponse: ' + charName + ': ' + response.data.items.averageItemLevelEquipped);
+    	return 10;
+    	
   });
 }
 
